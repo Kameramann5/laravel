f731ec1bd01a2dac3020a9cb5dd4f32968b19b7b
Kameramann5 <d.lisachyov2013@yandex.ru>
2024-10-31 01:47:18 +0300

    initial commit

---
+++ app/Models/Post.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use http\Env\Request;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Facades\Storage;
+
+class Post extends Model
+{
+    use Sluggable;
+    protected $fillable=['title','description','content','category_id','thumbnail'];
+    use HasFactory;
+    public function tags() {
+        return $this->belongsToMany(Tag::class)->withTimestamps();
+    }
+    public function  category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    //метод описывающий картинки, чтобы не указывать его сто раз везде
+    public static function uploadImage(Request $request, $image=null) {
+        if($request->hasFile('thumbnail')) {
+            if($image) {
+                Storage::delete($image);
+            }
+            $folder = date('Y-m-d');
+        return $request->file('thumbnail')->store("images/{$folder}");
+        }
+return null;
+    }
+
+public function getImage() {
+        if(!$this->thumbnail) {
+            return asset('no-image.png');
+        }
+        return asset("uploads/{$this->thumbnail}");
+}
+
+}
