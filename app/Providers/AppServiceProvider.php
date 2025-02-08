7e903e4360837b5eb176d4ce16cc2763b44c5463
Kameramann5 <d.lisachyov2013@yandex.ru>
2025-02-08 17:40:47 +0300

    кеш вывести список статей отсортировав по просмотрам, вывести список категорий и их количество

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,10 +25,18 @@ class AppServiceProvider extends ServiceProvider
         Paginator::defaultView('vendor.pagination.bootstrap-4');
 
         view()->composer('layouts.sidebar',function($view) {
+            //кеширование
+            if (Cache::has('cats')) {
+                $cats=Cache::get('cats');
+
+            } else {
+                $cats=Category::withCount('posts')->OrderBy('posts_count','desc')->get();
+                Cache::put('cats',$cats,30);
+            }
             //вывести список статей отсортировав по просмотрам
             $view->with('popular_posts',Post::orderBy('views','desc')->limit(3)->get());
             //вывести список категорий и их количество
-            $view->with('cats',Category::withCount('posts')->orderBy('posts_count','desc')->get());
+            $view->with('cats',$cats);
         });
 
 
