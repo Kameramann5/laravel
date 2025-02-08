87400c23dfe406d8944956227950bf36cf1c9d93
Kameramann5 <d.lisachyov2013@yandex.ru>
2025-02-08 17:33:26 +0300

    вывести список статей отсортировав по просмотрам, вывести список категорий и их количество

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::defaultView('vendor.pagination.bootstrap-4');
+
+        view()->composer('layouts.sidebar',function($view) {
+            //вывести список статей отсортировав по просмотрам
+            $view->with('popular_posts',Post::orderBy('views','desc')->limit(3)->get());
+            //вывести список категорий и их количество
+            $view->with('cats',Category::withCount('posts')->orderBy('posts_count','desc')->get());
+        });
+
+
+
     }
 }
